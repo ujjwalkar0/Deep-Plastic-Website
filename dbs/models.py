2216a0ff0b1d94cbd776663c1bf0377e7e97db01
Ujjwal Kar <ujjwalkar21@gmail.com>
2022-10-05 18:00:29 +0530

    ..

--- dbs/models.py
+++ dbs/models.py
@@ -16,25 +16,12 @@ class DailyStatistics(models.Model):
     def __str__(self):
         return f"{self.date} -- {self.location} -- {self.count}"
 
-# class LocationStatistics(models.Model):
-#     location = models.OneToOneField(User, on_delete=models.CASCADE)
-#     count = models.IntegerField(default=1)
-
-#     def __str__(self):
-#         return f"{self.location.username} -- {self.count}"
-
 class UploadImageTest(models.Model):
     image = models.ImageField(upload_to="Images/", blank=True, null=True)
     location = models.ForeignKey(User, on_delete=models.CASCADE)
     time = models.TimeField(default=datetime.datetime.now().strftime("%H:%M:%S"))
     date = models.DateField(default=datetime.date.today().strftime('%Y-%m-%d'), editable=False)
     
-    # def __str__(self):
-    #     return self.image
-
-    # class Meta:
-    #     unique_together = ('location', 'date')
-
     def save(self, *args, **kwargs):
         try:
             day = DailyStatistics.objects.get(date=self.date, location = self.location)
