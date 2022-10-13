4054d54f5588003375ab86d1354062540f35e759
Ujjwal Kar <ujjwalkar21@gmail.com>
2022-10-13 13:20:45 +0530

    ..

--- Nasa/settings.py
+++ Nasa/settings.py
@@ -79,21 +79,21 @@ WSGI_APPLICATION = 'Nasa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'deepocean', 
-#         'USER': 'ujjwal', 
-#         'PASSWORD': '123456',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'deepocean', 
+        'USER': 'ujjwal', 
+        'PASSWORD': '123456',
+    }
+}
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
